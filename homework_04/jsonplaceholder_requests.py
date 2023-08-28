7585cda3a049878fbfbcee023b7907f6a2d9e0c2
Nikita Demakov <demakoffnik@gmail.com>
2023-08-28 09:43:11 +0700

    add posts url and users url to jsonplaceholder_requests.py, init poetry

--- homework_04/jsonplaceholder_requests.py
+++ homework_04/jsonplaceholder_requests.py
@@ -2,5 +2,5 @@
 создайте асинхронные функции для выполнения запросов к ресурсам (используйте aiohttp)
 """
 
-USERS_DATA_URL = ""
-POSTS_DATA_URL = ""
+USERS_DATA_URL = "https://jsonplaceholder.typicode.com/users"
+POSTS_DATA_URL = "https://jsonplaceholder.typicode.com/posts"
